40dc44fb4cf9f259fd705dc2e888e43aab47cfbb
ANTHONUS <anthonus.d@gmail.com>
2024-10-19 15:46:55 +0200

    Graphics of the app

--- src/Application/AppController.java
+++ src/Application/AppController.java
@@ -1,5 +1,8 @@
 package Application;
 
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -7,10 +10,27 @@ import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
 
-
+    @FXML
+    public MFXTextField sourceLabel;
+    @FXML
+    public MFXTextField linkLabel;
+    @FXML
+    public MFXTextField infoLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    public void sourcePath(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void linkPath(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void createSymLink(ActionEvent actionEvent) {
     }
 }
