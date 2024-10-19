aa7076f8240932d5be0a76d3b55a117f5ecdc211
ANTHONUS <anthonus.d@gmail.com>
2024-10-19 22:12:02 +0200

    Update ripple animation
    Adding a private method to set a clip for the ripple animation to follow the radius of the button defined in the css
    putted the method to use in the initialize method.

--- src/Application/AppController.java
+++ src/Application/AppController.java
@@ -1,9 +1,11 @@
 package Application;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,10 +18,30 @@ public class AppController implements Initializable {
     public MFXTextField linkLabel;
     @FXML
     public MFXTextField infoLabel;
+    @FXML
+    public MFXButton symlinkButton;
+    @FXML
+    public MFXButton linkButton;
+    @FXML
+    public MFXButton sourceButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Add radius to buttons ripple animation
+        setClipButton(symlinkButton, 20);
+        setClipButton(linkButton, 5);
+        setClipButton(sourceButton, 5);
+    }
 
+    private void setClipButton (MFXButton button, int radius) {
+        Rectangle clip = new Rectangle();
+        clip.setArcWidth(radius*2);
+        clip.setArcHeight(radius*2);
+        button.setClip(clip);
+        button.layoutBoundsProperty().addListener((observable, oldValue, newValue) -> {
+            clip.setWidth(newValue.getWidth());
+            clip.setHeight(newValue.getHeight());
+        });
     }
 
     @FXML
